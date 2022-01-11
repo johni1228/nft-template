9ec291bf7e7484d9408a1acc235ef6447ebbf86f
johni1228 <akirus1228@gmail.com>
2022-01-11 23:24:52 +0900

    feat: init project

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ app.get('', function(req, res) {
     res.json({ message: 'welcome to computer NFT api' });
 });
 
-app.get('/images/:image', function(req, res) {
-    const imageName = req.params.image;
-    const imagePath = `${__dirname}/${dataDir}/images/${imageName}`;
-    console.log(imagePath);
-    if (fs.existsSync(imagePath)) {
-        return res.sendFile(imagePath);
-    } else {
-        return res.json({ message: "file not found" }, 404);
-    }
-});
-
 app.get('/api/v0/nfts/:nftId', function(req, res) {
     const nftId = parseInt(req.params.nftId) + 1;
     const jsonPath = `${dataDir}/json/${nftId}.json`;
@@ -28,8 +17,6 @@ app.get('/api/v0/nfts/:nftId', function(req, res) {
         const jsonBuffer = fs.readFileSync(jsonPath);
         const jsonString = jsonBuffer.toString();
         const jsonData = JSON.parse(jsonString);
-        jsonData.external_url = `${imageURI}/images/${nftId}.png`;
-        jsonData.image = `${imageURI}/images/${nftId}.png`;
         return res.json(jsonData);
     } catch (e) {
         console.log(e);
